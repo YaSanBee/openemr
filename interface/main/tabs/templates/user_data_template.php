a378e19b4d9e195c00740d76271ea25ab2c5392f
YaSanBee <nelsonli.zerg@gmail.com>
2025-04-19 19:54:35 +0800

    右上角，個人設定列表中，新增 YaSanBee About 頁面，選項
    點擊後，可以在中間，新增頁籤分頁，開啟 YaSanBee About 頁面

--- interface/main/tabs/templates/user_data_template.php
+++ interface/main/tabs/templates/user_data_template.php
@@ -33,6 +33,7 @@
                         <li class="menuLabel" data-bind="click: changeMFA"><i class="fa fa-fw pr-2 text-muted fa-key"></i>&nbsp;<?php echo xlt("MFA Management");?></li>
                         <div class="dropdown-divider"></div>
                         <li class="menuLabel" data-bind="click: function() {navigateTab('./../about_page.php', 'About', function() {activateTabByName('About',true);});}"><i class="fa fa-fw pr-2 text-muted fa-info"></i>&nbsp;<?php echo xlt("About");?> <?php echo text($GLOBALS['openemr_name']); ?></li>
+                        <li class="menuLabel" data-bind="click: function() {navigateTab('./../yasanbee_test_about_page.php', 'YaSanBee About', function() {activateTabByName('YaSanBee About',true);});}"><i class="fa fa-fw pr-2 text-muted fa-info"></i>&nbsp;<?php echo 'YaSanBee About';?> <?php echo text($GLOBALS['openemr_name']); ?></li>
                         <div class="dropdown-divider"></div>
                         <li class="menuLabel" data-bind="click: logout"><i class="fa fa-fw pr-2 text-muted fa-sign-out-alt"></i>&nbsp;<?php echo xlt("Logout");?></li>
                     </ul>
